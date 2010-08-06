493339be48cf8c67cc33ffa33f7ab8f3363682e8
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-05 21:34:45 -0500

    add in a favicon.ico url so i quit seeing dumb errors

--- urls.js
+++ urls.js
@@ -5,6 +5,11 @@ var escaperoute = require('escaperoute'),
 
 exports.patterns = routes('',
     url('^/blog/', 'blog/urls.patterns'),
-    url('^/$', 'blog/views.list_view', 'homepage')
+    url('^/$', 'blog/views.list_view', 'homepage'),
+    url('^/favicon.ico', function(req, resp) {
+        resp.writeHead(404, {'Content-Type':'text/html'});
+        resp.write("Not found.");
+        resp.end(); 
+    })
 )
 
