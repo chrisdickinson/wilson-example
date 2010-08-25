0ec707abb9a10d4572e4ec2b218f5b30edc235f4
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-25 18:08:03 -0500

    rework blog views, use project-level base.html

--- blog/views.js
+++ blog/views.js
@@ -3,35 +3,38 @@ var wilson = require('wilson'),
     renderToResponse = shortcuts.renderToResponse,
     getObjectOr404 = shortcuts.getObjectOr404;
 
-exports.author_list = function(request, response) {
+exports.author_list = function(request) {
     var Author = this.externals.auth.models.User;
     Author.objects.all(function(objects) {
-        renderToResponse(request, response)('blog/author_list.html', {
+        renderToResponse(request)('blog/author_list.html', {
             'author_list':objects
         });
     });
 };
 
-exports.author_entries = function(request, response, author_slug) {
+exports.author_entries = function(request, author_slug) {
     var Author = this.externals.auth.models.User;
-    getObjectOr404(response)(Author, {'username':author_slug}, function(author) {
-        renderToResponse(request, response)('blog/author_entries.html', {
+    getObjectOr404(request)(Author, {'username':author_slug}, function(author) {
+        renderToResponse(request)('blog/author_entries.html', {
             'author':author,
         });
     });
 };
 
-exports.detail_view = function(request, response, year, month, day, slug) {
+exports.detail_view = function(request, year, month, day, slug) {
     var Entry = this.models.Entry;
-    getObjectOr404(response)(Entry, {'slug':slug}, function(entry) {
-        renderToResponse(request, response)('blog/entry_detail.html', {'entry':entry});
+    getObjectOr404(request)(Entry, {'slug':slug}, function(entry) {
+        renderToResponse(request)('blog/entry_detail.html', {'entry':entry});
     });
 };
 
-exports.list_view = function(request, response) {
+exports.list_view = function(request) {
+    var sys = require('sys');
+    sys.debug(sys.inspect(request.get_user(this.externals.auth)));
+
     var Entry = this.models.Entry;
     Entry.objects.filter({}).limit(5).all(function(objects) {
-        renderToResponse(request, response)('blog/entry_list.html', {'entry_list':objects});
+        renderToResponse(request)('blog/entry_list.html', {'entry_list':objects});
     });    
 };
 
