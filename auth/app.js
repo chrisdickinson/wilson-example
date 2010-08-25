d00d3c4bab79d9e51a300838ef8a5e0524f3cd60
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-25 18:08:42 -0500

    remove old auth app

--- auth/app.js
+++
@@ -1,9 +0,0 @@
-var wilson = require('wilson'),
-    application = wilson.application,
-    app = application.app;
-
-exports.app = app({
-    'provides':'auth',
-    'models':require('./models'),
-    'external_apps':{},
-});
